1e000be634f35bb9d5444ff34decd831ebd05771
Risu <11910206@mail.sustech.edu.cn>
2021-04-09 19:20:44 +0800

    dicomment

--- 148.排序链表.java
+++ 148.排序链表.java
@@ -7,22 +7,22 @@
 // @lc code=start
 
 //  Definition for singly-linked list.
-// class ListNode {
-//     int val;
-//     ListNode next;
+class ListNode {
+    int val;
+    ListNode next;
 
-//     ListNode() {
-//     }
+    ListNode() {
+    }
 
-//     ListNode(int val) {
-//         this.val = val;
-//     }
+    ListNode(int val) {
+        this.val = val;
+    }
 
-//     ListNode(int val, ListNode next) {
-//         this.val = val;
-//         this.next = next;
-//     }
-// }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
     public static void main(String[] args) {
